b4f35b57a1cbc7a6dd1c5b086077ca1d6ef502c0
sai-charan2003 <saicharannallapaneni@gmail.com>
2026-09-17 18:11:48 -0400

    Update wear versionCode to 1 and versionName to 0.0.1

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.charan.batterytracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 1
+        versionName = "0.0.1"
         vectorDrawables {
             useSupportLibrary = true
         }
